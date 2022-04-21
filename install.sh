1a7262436b75c61c1364d89eb3743326b74bf5c4
Thai Nguyen <ducthaidev@gmail.com>
2022-04-21 14:41:06 +0700

    update

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ config_dir="${dir}/.config"
 #i3_config_dir="${config_dir}/i3"
 alacritty_config_dir="${config_dir}/alacritty"
 nvim_config_dir="${config_dir}/nvim"
+zathura_config_dir="${config_dir}/zathura"
 
+# Home
 if [ -f $tmux_conf_dir ]; then
     rm -rf $tmux_conf_dir
 fi
@@ -23,6 +25,7 @@ if [ -f $zprofile_dir ]; then
     rm -rf $zprofile_dir
 fi
 
+# XDG config
 if [ -d $alacritty_config_dir ]; then
     rm -rf $alacritty_config_dir
 fi
@@ -31,12 +34,18 @@ if [ -d $nvim_config_dir ]; then
     rm -rf $nvim_config_dir
 fi
 
+if [ -d $zathura_config_dir ]; then
+    rm -rf $zathura_config_dir
+fi
+
 dotfiles_dir=$(pwd)
 
 # create symbolic link of alacritty
 ln -sf $dotfiles_dir/.config/alacritty ${config_dir}
 # create symbolic link of neovim
 ln -sf $dotfiles_dir/.config/nvim ${config_dir}
+# create symbolic link of zathura
+ln -sf $dotfiles_dir/.config/zathura ${config_dir}
 
 # create symbolic link of zshrc
 ln -sf $dotfiles_dir/.zshrc $dir
