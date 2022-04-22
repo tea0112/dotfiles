5c4950a8ae8b7a765a482a171be30f59d5c7039c
Thai Nguyen <ducthaidev@gmail.com>
2022-04-22 10:42:57 +0700

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,14 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
 echo "Polybar launched..."
--- .zprofile
+++ .zprofile
@@ -1,3 +1,5 @@
+ibus-daemon -drxR
+
 if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
   exec startx
 fi
