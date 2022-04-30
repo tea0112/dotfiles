2405a85c5916b8742b8faef7a6af7583252094aa
Thai Nguyen <ducthaidev@gmail.com>
2022-04-30 11:56:37 +0700

    update

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -44,6 +44,8 @@ local lspconfig = require("lspconfig")
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities.offsetEncoding = { "utf-16" }
+
 
 ----------------------------
 -- Language Server Config --
