1bacf1092a6ea6201617d93b9ac45ef9f81cfe6f
ducthaidev <ducthaidev@gmail.com>
2022-05-16 08:29:58 +0700

    update

--- dependencies-install/neovim-installer.sh
+++ dependencies-install/neovim-installer.sh
@@ -11,3 +11,5 @@ sudo apt-get install -y ripgrep
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update -y
 sudo apt-get install -y neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
