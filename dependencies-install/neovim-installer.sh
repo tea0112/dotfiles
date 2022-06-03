814290eb3a910c53e3957453d7c64c2925df98bb
ducthaidev <ducthaidev@gmail.com>
2022-06-03 15:59:16 +0000

    update

--- dependencies-install/neovim-installer.sh
+++ dependencies-install/neovim-installer.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#sudo pacman -S gcc
-#sudo pacman -S ripgrep
-#sudo pacman -S neovim
+sudo pacman -S gcc
+sudo pacman -S ripgrep
+sudo pacman -S neovim
 
 rm -rf /tmp/nvm
 git clone https://aur.archlinux.org/nvm.git /tmp/nvm
 cd /tmp/nvm
 makepkg -si
-zsh
+cd
 rm -rf /tmp/nvm
 
-#nvim_config_dir="/home/$USER/nvim"
-#if [ -d "${nvim_config_dir}" ]; then
-#    rm -rf "${nvim_config_dir}"
-#fi
-#ln -sf /home/$USER/dotfiles/.config/nvim /home/$USER/.config
-#
-#Packer_File=/home/$USER/.local/share/nvim/site/pack/packer/start/packer.nvim
-#if [ ! -d "$Packer_File" ]; then
-#    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-#    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-#fi
-#
-#nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim_config_dir="/home/$USER/nvim"
+if [ -d "${nvim_config_dir}" ]; then
+    rm -rf "${nvim_config_dir}"
+fi
+ln -sf /home/$USER/dotfiles/.config/nvim /home/$USER/.config
+
+Packer_File=/home/$USER/.local/share/nvim/site/pack/packer/start/packer.nvim
+if [ ! -d "$Packer_File" ]; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
