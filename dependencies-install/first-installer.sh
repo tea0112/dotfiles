5845b47e7e82191e38314da78cbeca13a28b03ca
ducthaidev <ducthaidev@gmail.com>
2022-06-04 15:37:07 +0700

    update

--- dependencies-install/first-installer.sh
+++ dependencies-install/first-installer.sh
@@ -5,6 +5,7 @@ sudo pacman -Syu
 sudo pacman -S wget git linux linux-headers
 sudo pacman -S --needed base-devel
 sudo pacman -S polkit-gnome
+sudo pacman -S arandr
 
 bash ~/.scripts/mmi.sh https://aur.archlinux.org/yay
 bash ~/.scripts/mmi.sh https://aur.archlinux.org/google-chrome
