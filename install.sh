05d901b4560e21518d6edf6c60a943ecf54b1fd0
ducthaidev <ducthaidev@gmail.com>
2022-06-04 18:13:39 +0700

    update

--- install.sh
+++ install.sh
@@ -8,5 +8,3 @@ bash ~/dotfiles/config-data/zathura.sh
 bash ~/dotfiles/config-data/alacritty.sh
 bash ~/dotfiles/config-data/i3.sh
 bash ~/dotfiles/config-data/polybar.sh
-bash ~/.scripts/mmi.sh https://aur.archlinux.org/yay
-bash ~/.scripts/mmi.sh https://aur.archlinux.org/google-chrome
