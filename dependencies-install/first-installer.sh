2d74d34f065b72a145027510313dad2db5169c95
ducthaidev <ducthaidev@gmail.com>
2022-06-06 09:10:27 +0700

    update

--- dependencies-install/first-installer.sh
+++ dependencies-install/first-installer.sh
@@ -11,6 +11,11 @@ sudo pacman -S rofi
 sudo pacman -S noto-fonts-emoji
 sudo pacman -S i3lock
 sudo pacman -S picom
+sudo pacman -S zathura
+sudo pacman -S zathura-cb
+sudo pacman -S zathura-djvu
+sudo pacman -S zathura-pdf-poppler
+sudo pacman -S zathura-ps
 
 bash ~/dotfiles/.scripts/mmi.sh https://aur.archlinux.org/yay
 bash ~/dotfiles/.scripts/mmi.sh https://aur.archlinux.org/google-chrome
