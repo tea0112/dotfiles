3a00a1f35496d75382df96f81096b0abfce4d4ab
ducthaidev <ducthaidev@gmail.com>
2022-07-28 14:40:20 +0700

    update

---
+++ installation-scripts/imwheel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pacman -S imwheel
+
+if [ -f "~/.imwheelrc" ]; then
+    rm ~/.imwheelrc
+fi
+
+ln -sf ~/dotfiles/.imwheelrc ~/
--- installation-scripts/neovim-installer.sh
+++ installation-scripts/neovim-installer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#sudo pacman -S gcc
-#sudo pacman -S ripgrep
-#sudo pacman -S neovim
-#sudo pacman -S lua-language-server 
-#sudo pacman -S bash-language-server
+sudo pacman -S gcc
+sudo pacman -S ripgrep
+sudo pacman -S neovim
+sudo pacman -S lua-language-server 
+sudo pacman -S bash-language-server
 
 nvim_config_dir="/home/$USER/nvim"
 if [ -d "${nvim_config_dir}" ]; then
     rm -rf "${nvim_config_dir}"
 fi
-ln -sf /home/$USER/dotfiles/.config/nvim /home/$USER/.config
+ln -sf /home/$USER/dotfiles/.config/nvim/ /home/$USER/.config/
 
 Packer_File=/home/$USER/.local/share/nvim/site/pack/packer/start/packer.nvim
 if [ ! -d "$Packer_File" ]; then
