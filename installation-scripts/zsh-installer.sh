ad43c56f4270dee25cfa0c525c80f7952503a006
ducthaidev <ducthaidev@gmail.com>
2022-07-29 08:29:57 +0700

    update

--- installation-scripts/zsh-installer.sh
+++ installation-scripts/zsh-installer.sh
@@ -2,14 +2,14 @@
 
 #sudo pacman -S zsh
 
-#rm -rf ~/.oh-my-zsh
-#git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-#
-#rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-#git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-#
-#rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+rm -rf ~/.oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 rm -rf ~/.oh-my-zsh/custom/themes/powerlevel10k
 ZSH_CUSTOM=/home/$USER/.oh-my-zsh/custom
@@ -21,4 +21,4 @@ dotfiles_dir=~/dotfiles
 ln -sf ${dotfiles_dir}/.zshrc ~
 ln -sf ${dotfiles_dir}/.zprofile ~
 
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
