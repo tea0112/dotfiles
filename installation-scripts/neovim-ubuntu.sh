83d615c38442782a2b92738dfa4d26673a0e3c3b
ducthaidev <ducthaidev@gmail.com>
2022-08-01 15:45:38 +0700

    update

---
+++ installation-scripts/neovim-ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install build-essential -y
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
+nvim_config_dir="/home/$USER/nvim"
+if [ -d "${nvim_config_dir}" ]; then
+    rm -rf "${nvim_config_dir}"
+fi
+ln -sf /home/$USER/dotfiles/.config/nvim/ /home/$USER/.config/
+
+Packer_File=/home/$USER/.local/share/nvim/site/pack/packer/start/packer.nvim
+if [ ! -d "$Packer_File" ]; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
