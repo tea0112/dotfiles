dcef3a525318d308e07b563b1a2638adb784e536
tea0112 <ducthaidev@gmail.com>
2022-08-08 14:48:34 +0700

    update

--- .config/nvim/lua/bootstrap.lua
+++ .config/nvim/lua/bootstrap.lua
@@ -121,6 +121,8 @@ return require 'packer'.startup(function(use)
     use "windwp/nvim-autopairs"
 
     use 'elkowar/yuck.vim'
+    
+    use 'lambdalisue/suda.vim'
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
