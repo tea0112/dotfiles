ee5b825c0a191f918b09e09aceecfe5541fd94e8
tea0112 <ducthaidev@gmail.com>
2022-08-09 10:13:01 +0700

    update

---
+++ .scripts/set-touchpad.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Show list props
+# xinput list-props 16
+
+# tapping enabled
+xinput set-prop 16 304 1
+# natural scrolling enabled
+xinput set-prop 16 281 1
+# acceleration speed
+xinput set-prop 16 292 -0.3
+# acceleration profile
