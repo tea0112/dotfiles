acdba6fd1da841dc3dc01096cb0ba5248b4b26bd
tea0112 <ducthaidev@gmail.com>
2022-11-02 07:11:52 +0700

    update

--- scripts/map-caps-to-super-arch.sh
+++ scripts/map-caps-to-super-arch.sh
@@ -4,10 +4,12 @@ caps_lock_state=$(xset q | grep Caps | awk '{print $4}')
 
 if [[ ${caps_lock_state} == 'off' ]]; then
     setxkbmap -option caps:super
+    notify-send "DONE!"
 else
     if [[ ! $(bash ~/dotfiles/scripts/package-exist-arch.sh xdotool) ]]; then
         sudo pacman -S xdotool
     fi
     xdotool key Caps_Lock
     setxkbmap -option caps:super
+    notify-send "DONE!"
 fi
