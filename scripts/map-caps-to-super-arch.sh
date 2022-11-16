e55a3b7a5105607f3cc9d0a09c55785ecbdc33f8
tea0112 <ducthaidev@gmail.com>
2022-11-16 21:07:39 +0700

    update

--- scripts/map-caps-to-super-arch.sh
+++ scripts/map-caps-to-super-arch.sh
@@ -4,9 +4,13 @@ caps_lock_state=$(xset q | grep Caps | awk '{print $4}')
 
 if [[ ${caps_lock_state} == 'off' ]]; then
     setxkbmap -option caps:super
+    pkill picom
+    picom
     notify-send "DONE!"
 else
     xdotool key Caps_Lock
     setxkbmap -option caps:super
+    pkill picom
+    picom
     notify-send "DONE!"
 fi
