2bce9170bb4444b18582749a6348d48c503b6eb5
tea0112 <ducthaidev@gmail.com>
2022-11-23 21:37:54 +0700

    update

--- .config/nvim/lua/lvim/plugins/nvim_tree.lua
+++ .config/nvim/lua/lvim/plugins/nvim_tree.lua
@@ -2,6 +2,9 @@ local M = {}
 
 function M.setup()
 	require("nvim-tree").setup({
+		view = {
+			adaptive_size = true,
+		},
 		actions = {
 			open_file = {
 				quit_on_open = true,
@@ -11,6 +14,9 @@ function M.setup()
 			enable = true,
 			ignore = false,
 			timeout = 500,
+		},
+		renderer = {
+			group_empty = true,
 		},
 	})
 end
