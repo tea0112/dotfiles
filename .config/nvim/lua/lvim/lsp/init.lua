4822d51c74c794635eae5d9faa4a298abaf99a7d
tea0112 <ducthaidev@gmail.com>
2022-11-25 08:11:05 +0700

    update

--- .config/nvim/lua/lvim/lsp/init.lua
+++ .config/nvim/lua/lvim/lsp/init.lua
@@ -46,8 +46,4 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
 
-require("nvim-lsp-installer").setup({
-	automatic_installation = true,
-})
-
 require("lvim.lsp.configs").setup(lspconfig, on_attach, capabilities)
