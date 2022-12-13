a7f99b0401bebf1812ddefaf9dd05dcc7ca3c27f
tea0112 <ducthaidev@gmail.com>
2022-12-13 08:03:44 +0700

    update

--- installation-scripts/neovim-ubuntu.sh
+++ installation-scripts/neovim-ubuntu.sh
@@ -5,30 +5,23 @@ sudo apt update
 sudo apt install neovim -y
 sudo apt install fd-find -y
 sudo apt install ripgrep -y
-sudo apt install flake8 -y
 sudo apt install python3-pip -y
-sudo apt install -y luarocks
-sudo luarocks install luacheck
-pip install --upgrade autopep8
-pip install cmake-language-server
 
 nvim_config_dir="/home/$USER/.config/nvim"
 if [ -d "${nvim_config_dir}" ]; then
-    rm -rf "${nvim_config_dir}"
+	rm -rf "${nvim_config_dir}"
 else
-    mkdir -p "/home/$USER/.config/"
+	mkdir -p "/home/$USER/.config/"
 fi
 ln -sf /home/$USER/dotfiles/.config/nvim/ /home/$USER/.config/
 
 packer=/home/$USER/.local/share/nvim/site/pack/packer
 if [ -d "$packer" ]; then
-    echo "detect"
-    rm -rf "${packer}"
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	echo "detect"
+	rm -rf "${packer}"
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 else
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
