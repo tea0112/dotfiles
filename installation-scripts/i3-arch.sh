c6ff14327ab4ab224d6d4a3abf561ac86aea6303
tea0112 <ducthaidev@gmail.com>
2023-02-04 10:28:03 +0700

    update

--- installation-scripts/i3-arch.sh
+++ installation-scripts/i3-arch.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+sudo pacman -S wmctrl
 sudo pacman -S xdotool
+sudo pacman -S lxqt-policykit
+sudo pacman -S network-manager-applet
+sudo pacman -S blueman
+sudo pacman -S autorandr
+sudo pacman -S feh
+sudo pacman -S xfce4-notifyd
+sudo pacman -S ttf-font-awesome
 
 if [ -d "/home/thai/.config/i3" ]; then
     rm -rf ~/.config/i3
