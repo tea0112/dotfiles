532eff70616a8188a0fbdd09bffebbc63153ac1e
tea0112 <ducthaidev@gmail.com>
2023-03-14 11:48:05 +0700

    update

--- .config/nvim/lua/lazy_plugins.lua
+++ .config/nvim/lua/lazy_plugins.lua
@@ -1,145 +1,147 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	"williamboman/mason-lspconfig.nvim",
-	"williamboman/mason.nvim",
-	"neovim/nvim-lspconfig",
-	"jay-babu/mason-null-ls.nvim",
-	"max397574/better-escape.nvim",
-	{
-		"kyazdani42/nvim-tree.lua",
-		dependencies = {
-			"kyazdani42/nvim-web-devicons", -- optional, for file icon
-		},
-		tag = "nightly", -- optional, updated every week. (see issue #1193)
-	},
-	-- vim surround
-	"tpope/vim-surround",
-	-- easymotion
-	"easymotion/vim-easymotion",
-	-- fzf
-	"junegunn/fzf",
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-	},
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	-- cmp
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/nvim-cmp", -- Autocompletion plugin
-	"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
-	-- lualine
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
-	},
-	-- null-ls
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	-- git change show
-	{
-		"lewis6991/gitsigns.nvim",
-		tag = "release",
-	},
-	-- git fugitive
-	"tpope/vim-fugitive",
-	-- markdown glow
-	{ "ellisonleao/glow.nvim", branch = "main" },
+    "rcarriga/nvim-notify",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "williamboman/mason-lspconfig.nvim",
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    "jay-babu/mason-null-ls.nvim",
+    "max397574/better-escape.nvim",
+    {
+        "kyazdani42/nvim-tree.lua",
+        dependencies = {
+            "kyazdani42/nvim-web-devicons", -- optional, for file icon
+        },
+        tag = "nightly",           -- optional, updated every week. (see issue #1193)
+    },
+    -- vim surround
+    "tpope/vim-surround",
+    -- easymotion
+    "easymotion/vim-easymotion",
+    -- fzf
+    "junegunn/fzf",
+    -- telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    -- cmp
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",      -- Autocompletion plugin
+    "hrsh7th/cmp-nvim-lsp",  -- LSP source for nvim-cmp
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+    -- lualine
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
+    },
+    -- null-ls
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    -- git change show
+    {
+        "lewis6991/gitsigns.nvim",
+        tag = "release",
+    },
+    -- git fugitive
+    "tpope/vim-fugitive",
+    -- markdown glow
+    { "ellisonleao/glow.nvim",           branch = "main" },
 
-	-- css color
-	"norcalli/nvim-colorizer.lua",
+    -- css color
+    "norcalli/nvim-colorizer.lua",
 
-	-- Fterm
-	"numToStr/FTerm.nvim",
+    -- Fterm
+    "numToStr/FTerm.nvim",
 
-	-- vim go
-	"fatih/vim-go",
+    -- vim go
+    "fatih/vim-go",
 
-	-- which key
-	"folke/which-key.nvim",
+    -- which key
+    "folke/which-key.nvim",
 
-	"leoluz/nvim-dap-go",
-	{
-		"nvim-telescope/telescope-dap.nvim",
-	},
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"Pocco81/DAPInstall.nvim",
-			"theHamsta/nvim-dap-virtual-text",
-			"rcarriga/nvim-dap-ui",
-			{ "jbyuki/one-small-step-for-vimkind", module = "osv" },
-		},
-	},
+    "leoluz/nvim-dap-go",
+    {
+        "nvim-telescope/telescope-dap.nvim",
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "Pocco81/DAPInstall.nvim",
+            "theHamsta/nvim-dap-virtual-text",
+            "rcarriga/nvim-dap-ui",
+            { "jbyuki/one-small-step-for-vimkind", module = "osv" },
+        },
+    },
 
-	"antoinemadec/FixCursorHold.nvim",
+    "antoinemadec/FixCursorHold.nvim",
 
-	"kosayoda/nvim-lightbulb",
+    "kosayoda/nvim-lightbulb",
 
-	{
-		"rmagatti/auto-session",
-		config = function()
-			require("auto-session").setup({
-				log_level = "info",
-				auto_session_suppress_dirs = { "~/", "~/Projects" },
-			})
-		end,
-	},
+    {
+        "rmagatti/auto-session",
+        config = function()
+            require("auto-session").setup({
+                log_level = "info",
+                auto_session_suppress_dirs = { "~/", "~/Projects" },
+            })
+        end,
+    },
 
-	"lukas-reineke/indent-blankline.nvim",
+    "lukas-reineke/indent-blankline.nvim",
 
-	"rafamadriz/friendly-snippets",
+    "rafamadriz/friendly-snippets",
 
-	"windwp/nvim-autopairs",
+    "windwp/nvim-autopairs",
 
-	"elkowar/yuck.vim",
+    "elkowar/yuck.vim",
 
-	"lambdalisue/suda.vim",
+    "lambdalisue/suda.vim",
 
-	"preservim/nerdcommenter",
-	{
-		"crusj/structrue-go.nvim",
-		branch = "main",
-	},
-	{
-		"crusj/hierarchy-tree-go.nvim",
-		dependencies = "neovim/nvim-lspconfig",
-	},
-	"navarasu/onedark.nvim",
-	{
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
-	"folke/tokyonight.nvim",
-	"EdenEast/nightfox.nvim",
-	"vimwiki/vimwiki",
-	{
-		"mfussenegger/nvim-jdtls",
-		ft = { "java" },
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-	},
+    "preservim/nerdcommenter",
+    {
+        "crusj/structrue-go.nvim",
+        branch = "main",
+    },
+    {
+        "crusj/hierarchy-tree-go.nvim",
+        dependencies = "neovim/nvim-lspconfig",
+    },
+    "navarasu/onedark.nvim",
+    {
+        "iamcco/markdown-preview.nvim",
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
+    "folke/tokyonight.nvim",
+    "EdenEast/nightfox.nvim",
+    "vimwiki/vimwiki",
+    {
+        "mfussenegger/nvim-jdtls",
+        ft = { "java" },
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+    },
 }
 
 local opts = {}
--- .config/nvim/lua/plugins/nvim_cmp.lua
+++ .config/nvim/lua/plugins/nvim_cmp.lua
@@ -1,63 +1,72 @@
 local M = {}
 function M.setup()
-	--require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./lua/snippets" } })
-	require("luasnip.loaders.from_vscode").lazy_load()
-	-- nvim-cmp setup
-	local cmp = require("cmp")
-	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-	local luasnip = require("luasnip")
+    --require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./lua/snippets" } })
+    require("luasnip.loaders.from_vscode").lazy_load()
+    -- nvim-cmp setup
+    local cmp = require("cmp")
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local luasnip = require("luasnip")
 
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		},
-		mapping = {
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.close(),
-			["<CR>"] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true,
-			}),
-			["<Tab>"] = function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				else
-					fallback()
-				end
-			end,
-			["<S-Tab>"] = function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end,
-		},
-		sources = {
-			{ name = "nvim_lsp" },
-			{ name = "luasnip" },
-		},
-	})
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        mapping = {
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.close(),
+            ["<CR>"] = cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            }),
+            ["<Tab>"] = function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end,
+            ["<S-Tab>"] = function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end,
+        },
+        sources = {
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+        },
+    })
 
-	-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-	cmp.setup.cmdline(":", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "path" },
-		}, {
-			{ name = "cmdline" },
-		}),
-	})
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = "path" },
+        }, {
+            { name = "cmdline" },
+        }),
+    })
+
+    -- lsp_document_symbols
+    cmp.setup.cmdline("/", {
+        sources = cmp.config.sources({
+            { name = "nvim_lsp_document_symbol" },
+        }, {
+            { name = "buffer" },
+        }),
+    })
 end
 
 return M
