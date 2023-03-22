275bbc83aa074ab92446ef8c375e71d0868caa30
tea0112 <ducthaidev@gmail.com>
2023-03-22 14:54:58 +0700

    update

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -5,25 +5,25 @@ function M.setup()
     if not ok then
         print("Something went wrong", npairs)
         return
-    else
-        npairs.setup({
-            disable_filetype = { "TelescopePrompt" },
-            fast_wrap = {
-                map = "<M-e>",
-                chars = { "{", "[", "(", '"', "'" },
-                pattern = [=[[%'%"%)%>%]%)%}%,]]=],
-                end_key = "$",
-                keys = "qwertyuiopzxcvbnmasdfghjkl",
-                check_comma = true,
-                highlight = "Search",
-                highlight_grey = "Comment",
-            },
-        })
-
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end
+
+    npairs.setup({
+        disable_filetype = { "TelescopePrompt" },
+        fast_wrap = {
+            map = "<M-e>",
+            chars = { "{", "[", "(", '"', "'" },
+            pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+            end_key = "$",
+            keys = "qwertyuiopzxcvbnmasdfghjkl",
+            check_comma = true,
+            highlight = "Search",
+            highlight_grey = "Comment",
+        },
+    })
+
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return M
