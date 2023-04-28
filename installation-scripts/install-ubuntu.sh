b118dfcb92b929e97ec494c8be4714a867c1446d
tea0112 <ducthaidev@gmail.com>
2023-04-28 18:49:14 +0700

    update

--- installation-scripts/install-ubuntu.sh
+++ installation-scripts/install-ubuntu.sh
@@ -1,34 +1,60 @@
 #!/bin/bash
 
-echo "******************* Install Tmux *******************"
-rm -rf ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "_____________________________________________________"
+read -p "Download and Install startship?" confirm
+case $confirm in
+y)
+	curl -sS https://starship.rs/install.sh | sh
+	;;
+*)
+	echo "you chose NO"
+	;;
+esac
 
+echo "_____________________________________________________"
+read -p "Clone tmux tpm plugin?" confirm
+case $confirm in
+y)
+	rm -rf ~/.tmux/plugins/tpm
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	;;
+*)
+	echo "you chose NO"
+	;;
+esac
+echo "******************* Config Tmux *******************"
 rm -rf ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux.conf ~
 
-echo "******************* Install Starship *******************"
-curl -sS https://starship.rs/install.sh | sh
-
+echo "******************* Config Starship *******************"
 if [ -d "${HOME}/.config/starship.toml" ]; then
 	rm -rf ~/.config/starship.toml
 fi
 
 ln -sf ~/dotfiles/.config/startship.toml ~/.config/
 
-echo "******************* Install Zsh *******************"
-rm -rf ~/.oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+echo "_____________________________________________________"
+read -p "Clone zsh ohmyzsh, autosuggestions, zsh-syntax-highlighting, powerlevel10k?" confirm
+case $confirm in
+y)
+	rm -rf ~/.oh-my-zsh
+	git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
-rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+	rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-rm -rf ~/.oh-my-zsh/custom/themes/powerlevel10k
+	rm -rf ~/.oh-my-zsh/custom/themes/powerlevel10k
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
+	;;
+*)
+	echo "you chose NO"
+	;;
+esac
+echo "******************* Config Zsh *******************"
 ZSH_CUSTOM=/home/$USER/.oh-my-zsh/custom
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
 
 rm -rf ~/.zshrc
 rm -rf ~/.zprofile
@@ -39,7 +65,7 @@ ln -sf ${dotfiles_dir}/.zprofile ~
 
 chsh -s $(which zsh)
 
-echo "******************* Install Neovim *******************"
+echo "******************* Config Neovim *******************"
 nvim_config_dir="/home/$USER/.config/nvim"
 if [ -d "${nvim_config_dir}" ]; then
 	rm -rf "${nvim_config_dir}"
@@ -48,21 +74,21 @@ ln -sf /home/$USER/dotfiles/.config/nvim/ /home/$USER/.config/
 ln -sf $HOME/dotfiles/.shellcheckrc $HOME
 ln -sf $HOME/dotfiles/.ideavimrc $HOME
 
-echo "******************* Install Polybar *******************"
+echo "******************* Config Polybar *******************"
 if [ -d "~/.config/polybar" ]; then
 	rm -rf ~/.config/polybar
 fi
 
 ln -sf ~/dotfiles/.config/polybar/ ~/.config/
 
-echo "******************* Install Rofi *******************"
+echo "******************* Config Rofi *******************"
 if [ -d "~/.config/rofi" ]; then
 	rm -rf ~/.config/rofi
 fi
 
 ln -sf ~/dotfiles/.config/rofi/ ~/.config/
 
-echo "******************* Install I3 *******************"
+echo "******************* Config I3 *******************"
 # rofimoji
 # arandr
 # gnome-disk-utility
@@ -81,26 +107,29 @@ fi
 
 ln -sf ~/dotfiles/.config/i3/ ~/.config/
 
-echo "******************* Install Imwheel *******************"
+echo "******************* Config Imwheel *******************"
 if [ -f "~/.imwheelrc" ]; then
 	rm ~/.imwheelrc
 fi
 
 ln -sf ~/dotfiles/.imwheelrc ~/
 
-echo "******************* Install Mpv *******************"
+echo "******************* Config Mpv *******************"
 rm -rf ~/.config/mpv
 ln -sf ~/dotfiles/.config/mpv/ ~/.config
 
-echo "========== Configure zathura =========="
+echo "========== Config zathura =========="
 if [[ -d "$HOME/.config/zathura " ]]; then
 	rm -rf ~/.config/zathura
 fi
 
 ln -sf ~/dotfiles/.config/zathura/ ~/.config
 
-echo "******************* Install Font *******************"
-if [ "$1" = "font" ]; then
+echo "_____________________________________________________"
+read -p "Install Jetbrains Font?" confirm
+case $confirm in
+y)
+	echo "******************* Install Font *******************"
 	FONT_DIR="$HOME/.fonts"
 	if [ -d "$FONT_DIR" ]; then
 		cp $HOME/dotfiles/JetBrains\ Mono\ Nerd\ Font\ Complete\ Regular.ttf $FONT_DIR
@@ -110,4 +139,8 @@ if [ "$1" = "font" ]; then
 		cp $HOME/dotfiles/JetBrains\ Mono\ Nerd\ Font\ Complete\ Regular.ttf $FONT_DIR
 		fc-cache -f -v
 	fi
-fi
+	;;
+*)
+	echo "you chose NO"
+	;;
+esac
--- installation-scripts/ubuntu-package.sh
+++ installation-scripts/ubuntu-package.sh
@@ -2,55 +2,152 @@
 
 if [[ ! -f "/usr/bin/zsh" ]]; then
     echo "ZSH doesn't exist"
-    exit 0;
+    echo "You should run this script by ZSH!"
+    exit 0
 fi
 
-sudo locale-gen "en_US.UTF-8"
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl jq wget fd-find ripgrep python3-pip wmctrl python3-pip python3-virtualenv python3-venv xclip aria2 unzip build-essential zsh tmux xsel neovim batcat -y
+# template
+#echo "_____________________________________________________"
+#read -p "?" confirm
+#case $confirm in
+#y)
+#    ;;
+#*)
+#    echo "you chose NO"
+#    ;;
+#esac
+
+echo "_____________________________________________________"
+read -p "Generate en_US.UTF-8?" confirm
+case $confirm in
+y)
+    sudo locale-gen "en_US.UTF-8"
+    ;;
+*)
+    echo "You didn't choose!"
+    ;;
+esac
+
+echo "_____________________________________________________"
+read -p "Add neovim unstable ppa?" confirm
+case $confirm in
+y)
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt update && sudo apt upgrade -y
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
+
+echo "_____________________________________________________"
+read -p "Install package?" confirm
+case $confirm in
+y)
+    sudo apt install curl jq wget fd-find ripgrep python3-pip wmctrl python3-pip python3-virtualenv python3-venv xclip aria2 unzip build-essential zsh tmux xsel neovim -y
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
 
 # nvm
-get_repo_version="$HOME/dotfiles/scripts/get_repo_version.sh"
-version=$($get_repo_version nvm-sh/nvm)
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${version}/install.sh" | bash
+echo "_____________________________________________________"
+read -p "Install nvm?" confirm
+case $confirm in
+y)
+    get_repo_version="$HOME/dotfiles/scripts/get_repo_version.sh"
+    version=$($get_repo_version nvm-sh/nvm)
+    curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${version}/install.sh" | bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm i node
-nvm use node
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
+
+echo "_____________________________________________________"
+read -p "Install nodejs with nvm?" confirm
+case $confirm in
+y)
+    nvm i node
+    nvm use node
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
 
 # Go
-sudo whoami
-version=$(curl -s https://go.dev/dl/ | grep 'go.*.linux-amd64.tar.gz' -m1 -o)
-wget -O "/tmp/$version" "https://dl.google.com/go/${version}"
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "/tmp/$version"
-export PATH=$PATH:/usr/local/go/bin
+echo "_____________________________________________________"
+read -p "Install golang?" confirm
+case $confirm in
+y)
+    sudo whoami
+    version=$(curl -s https://go.dev/dl/ | grep 'go.*.linux-amd64.tar.gz' -m1 -o)
+    wget -O "/tmp/$version" "https://dl.google.com/go/${version}"
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "/tmp/$version"
+    export PATH=$PATH:/usr/local/go/bin
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "/home/${USER}/.cargo/env"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source "/home/${USER}/.cargo/env"
 
-cargo install exa
-cargo install zoxide
+    cargo install exa
+    cargo install zoxide
+    cargo install bat
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
 
-pip install cmake
+echo "_____________________________________________________"
+read -p "cmake?" confirm
+case $confirm in
+y)
+    pip install cmake
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
 
 # docker
-#if [ "$1" = "docker" ]; then
-#	echo "ok"
-#	sudo apt-get install \
-#		ca-certificates \
-#		curl \
-#		gnupg \
-#		lsb-release
-#
-#	sudo mkdir -m 0755 -p /etc/apt/keyrings
-#	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#	echo \
-#		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-#	sudo apt-get update
-#	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-#	sudo usermod -aG docker thai
-#	sudo docker run -d --restart=always -e 'SILENT=true' -p 56789:8000 sadeghhayeri/green-tunnel
-#fi
+echo "_____________________________________________________"
+read -p "docker ?" confirm
+case $confirm in
+y)
+    echo "ok"
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    read -p "enter the user for docker group: " user
+    sudo usermod -aG docker $user
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
+
+echo "_____________________________________________________"
+read -p "greentunnel?" confirm
+case $confirm in
+y)
+    sudo docker run -d --restart=always -e 'SILENT=true' -p 56789:8000 sadeghhayeri/green-tunnel
+    ;;
+*)
+    echo "you chose NO"
+    ;;
+esac
