70addbc9f7c1d617175fc87972b3b532b9f97412
tea0112 <ducthaidev@gmail.com>
2023-04-30 07:11:04 +0700

    update

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,37 +1,38 @@
 local M = {}
 
 function M.setup()
-	require("mason").setup()
+    require("mason").setup()
 
-	require("mason-lspconfig").setup({
-		ensure_installed = {
+    require("mason-lspconfig").setup({
+        ensure_installed = {
             "pyright",
-			"bashls",
-			"lua_ls",
-			"rust_analyzer",
-			"tailwindcss",
-			"emmet_ls",
-			"html",
-			"bufls",
-			"yamlls",
-			"cmake",
-			"marksman",
-			"tsserver",
-		},
-	})
+            "bashls",
+            "lua_ls",
+            "rust_analyzer",
+            "tailwindcss",
+            "emmet_ls",
+            "html",
+            "bufls",
+            "yamlls",
+            "cmake",
+            "marksman",
+            "tsserver",
+            "angularls",
+        },
+    })
 
-	require("mason-null-ls").setup({
-		ensure_installed = {
+    require("mason-null-ls").setup({
+        ensure_installed = {
             "google-java-format",
-			"shfmt",
-			"shellcheck",
-			"prettier",
-			"buf",
-			"stylua",
-			"autopep8",
-			"flake8",
-		},
-	})
+            "shfmt",
+            "shellcheck",
+            "prettier",
+            "buf",
+            "stylua",
+            "autopep8",
+            "flake8",
+        },
+    })
 end
 
 return M
