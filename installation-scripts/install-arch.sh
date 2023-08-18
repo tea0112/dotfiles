65447424a95772e62e42cd4bf5f675b3e3210efe
tea0112 <ducthaidev@gmail.com>
2023-08-18 15:53:55 +0700

    update

--- installation-scripts/install-arch.sh
+++ installation-scripts/install-arch.sh
@@ -80,15 +80,6 @@ fi
 
 ln -sf ~/dotfiles/.config/zathura/ ~/.config
 
-echo "========== Install nvm =========="
-get_repo_version="$HOME/dotfiles/scripts/get_repo_version.sh"
-version=$($get_repo_version nvm-sh/nvm)
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${version}/install.sh" | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install --lts
-nvm use --lts
-
 echo "========== Install Neovim =========="
 nvim_config_dir="/home/$USER/.config/nvim"
 if [ -d "${nvim_config_dir}" ]; then
---
+++ installation-scripts/install-nvm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "========== Install nvm =========="
+get_repo_version="$HOME/dotfiles/scripts/get_repo_version.sh"
+version=$($get_repo_version nvm-sh/nvm)
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${version}/install.sh" | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
+nvm use --lts
+
