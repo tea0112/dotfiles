d7b3fbdf9e566ff648cbe7544f0fa52d628d822b
tea0112 <ducthaidev@gmail.com>
2023-08-21 08:05:50 +0700

    update

--- installation-scripts/install-arch.sh
+++ installation-scripts/install-arch.sh
@@ -9,7 +9,7 @@ fi
 echo "instal default terminal for things like ranger, neovim"
 echo "export TERMINAL=/usr/bin/alacritty" >> $HOME/.profile
 
-sudo pacman -S curl mpv gcc wget ripgrep rofi polybar jq aria2 exa xclip fd zsh zoxide starship ripgrep zathura zathura-cb zathura-djvu zathura-ps zathura-pdf-mupdf xsel rofimoji arandr gnome-disk-utility wmctrl xdotool lxqt-policykit network-manager-applet blueman autorandr feh xfce4-notifyd ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra vlc neovim telegram-desktop thunar qbittorrent unzip gnome-disk-utility git-delta bat dust gnome-keyring okular breeze copyq gvfs-mtp zip
+sudo pacman -S curl mpv gcc wget ripgrep rofi polybar jq aria2 exa xclip fd zsh zoxide starship ripgrep zathura zathura-cb zathura-djvu zathura-ps zathura-pdf-mupdf xsel rofimoji arandr gnome-disk-utility wmctrl xdotool lxqt-policykit network-manager-applet blueman autorandr feh xfce4-notifyd ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra vlc neovim telegram-desktop thunar qbittorrent unzip gnome-disk-utility git-delta bat dust gnome-keyring okular breeze copyq gvfs-mtp zip ueberzug
 
 echo "========== Configure ZSH =========="
 rm -rf ~/.oh-my-zsh
@@ -32,6 +32,15 @@ ln -sf ${dotfiles_dir}/.zshrc ~
 ln -sf ${dotfiles_dir}/.zprofile ~
 
 chsh -s $(which zsh)
+
+echo ".------------------------------------------------."
+echo "|                     ranger                     |"
+echo "'------------------------------------------------'"
+ranger_directory="$HOME/.config/ranger"
+if [ -d "$ranger_directory" ]; then
+    rm -rf "$ranger_directory" 
+fi
+ln -sf "$HOME/dotfiles/.config/ranger/" "$XDG_CONFIG_HOME/"
 
 echo "========== Install font =========="
 FONT_DIR="$HOME/.fonts"
