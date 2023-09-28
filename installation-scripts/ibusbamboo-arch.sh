6e629ec92fa3e456f9754bdf425e03e7433aeaef
tea0112 <ducthaidev@gmail.com>
2023-09-28 11:18:46 +0700

    update

--- installation-scripts/ibusbamboo-arch.sh
+++ installation-scripts/ibusbamboo-arch.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/BambooEngine/ibus-bamboo/master/archlinux/install.sh)"
-
-sudo bash -c 'echo "export GTK_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export QT_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export XMODIFIERS=@im=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export QT4_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export CLUTTER_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "ibus-daemon -drx" >> "/etc/profile"'
