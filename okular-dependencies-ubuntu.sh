6c5a8f22e85f3eeb27e18653688e72278f2b20e4
tea0112 <ducthaidev@gmail.com>
2023-09-28 10:26:58 +0700

    update

--- okular-dependencies-ubuntu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y extra-cmake-modules qtbase5-dev qtdeclarative5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5svg5-dev libqt5texttospeech5-dev libpoppler-dev libspectre-dev libchm-dev libzip-dev libdjvulibre-dev libepub-dev libmarkdown2-dev
