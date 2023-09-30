ac60d3d33af5853b7f23a9f900a59616194b5c29
tea0112 <ducthaidev@gmail.com>
2023-09-30 09:12:34 +0700

    update

--- scripts/toggle-goldendict.sh
+++ scripts/toggle-goldendict.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-wmctrl -a 'GoldenDict'
+
+WINDOW_ID=$(xdotool getactivewindow)
+CLASS_NAME=$(xprop -id $WINDOW_ID | rg WM_CLASS | cut -d '"' -f2)
+
+# notify-send "CLASS_NAME" "$CLASS_NAME"
+
+if [[ $CLASS_NAME == "goldendict" ]]; then
+	xdotool windowminimize "$(xdotool getactivewindow)"
+else
+	wmctrl -a "Goldendict"
+fi
