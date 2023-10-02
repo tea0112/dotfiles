3785d0e7dc810d143323671e21601bb264bd2571
tea0112 <ducthaidev@gmail.com>
2023-10-02 21:29:37 +0700

    update

--- scripts/local_repos_status.sh
+++ scripts/local_repos_status.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo '-----------------------'
-echo '----  nvim status  ----'
-echo '-----------------------'
+echo '.---------------------.'
+echo '|     nvim status     |'
+echo '.---------------------.'
 cd ~/.config/nvim || exit
 git status
-echo '-----------------------'
-echo '--  dotfiles status  --'
-echo '-----------------------'
+echo '.---------------------.'
+echo '|   dotfiles status   |'
+echo '.---------------------.'
 cd ~/dotfiles || exit
 git status
