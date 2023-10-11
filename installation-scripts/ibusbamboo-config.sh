df4e2ab72388ea70f7423d73ca440c14ee50307c
tea0112 <ducthaidev@gmail.com>
2023-10-11 09:18:57 +0700

    update

--- installation-scripts/ibusbamboo-config.sh
+++ installation-scripts/ibusbamboo-config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo bash -c 'echo "export GTK_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export QT_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export XMODIFIERS=@im=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export QT4_IM_MODULE=ibus" >> "/etc/profile"'
-sudo bash -c 'echo "export CLUTTER_IM_MODULE=ibus" >> "/etc/profile"'
+sudo bash -c 'echo "GTK_IM_MODULE=ibus" >> "/etc/environment"'
+sudo bash -c 'echo "QT_IM_MODULE=ibus" >> "/etc/environment"'
+sudo bash -c 'echo "XMODIFIERS=@im=ibus" >> "/etc/environment"'
+sudo bash -c 'echo "QT4_IM_MODULE=ibus" >> "/etc/environment"'
+sudo bash -c 'echo "CLUTTER_IM_MODULE=ibus" >> "/etc/environment"'
+sudo bash -c 'echo "GLFW_IM_MODULE=ibus" >> "/etc/environment"'
 sudo bash -c 'echo "ibus-daemon -drx" >> "/etc/profile"'
