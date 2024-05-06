de447cf1032c696901275ca6f4f73df851400665
tea0112 <ducthaidev@gmail.com>
2024-05-06 07:46:49 +0700

    update

--- scripts/local_repos_status.sh
+++ scripts/local_repos_status.sh
@@ -4,9 +4,12 @@ echo '.---------------------.'
 echo '|     nvim status     |'
 echo '.---------------------.'
 cd ~/dotfiles/.config/nvim || exit
+git pull
 git status
+
 echo '.---------------------.'
 echo '|   dotfiles status   |'
 echo '.---------------------.'
 cd ~/dotfiles || exit
+git pull
 git status
