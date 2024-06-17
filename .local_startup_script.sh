a01fdcd7850cf61a0c7e30e65788dafe30ab5ba1
tea0112 <ducthaidev@gmail.com>
2024-06-17 09:32:21 +0700

    update

---
+++ .local_startup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias dvdrental="PGPASSWORD=Postgres\!23456 psql -h localhost -p 5432 -U postgres -d dvdrental"
+chmod 600 ~/.ssh/*
+
+if [ -z "$SSH_AGENT_PID" ]; then
+    eval $(ssh-agent -s)
+    ssh-add -K ~/.ssh/github-tea0112 ~/.ssh/zlp_rsa
+fi
