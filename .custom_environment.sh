26377b05f0973cc1c4e90920620542b21955736b
tea <ducthaidev@gmail.com>
2024-10-06 11:21:27 +0700

    update

--- .custom_environment.sh
+++ .custom_environment.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-alias dvdrental="PGPASSWORD=Postgres\!23456 psql -h localhost -p 5432 -U postgres -d dvdrental"
-chmod 600 ~/.ssh/*
+# go install golang.org/dl/go1.21.0@latest
+# go1.21.0 download
+# go1.21.0 version
+alias go="go1.21.0"
 
-if [ -z "$SSH_AGENT_PID" ]; then
-    eval $(ssh-agent -s)
-    ssh-add ~/.ssh/github-tea0112 ~/.ssh/zlp_rsa
-fi
+export PGPASSWD=Postgres!23456
+export PGUSER=postgres
+export PGPORT=5432
+export PGDB=postgres
+export GOPRIVATE=dev.azure.com
