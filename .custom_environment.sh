a2e84d5d6cda693707798a571a8034a3aece6f10
tea <ducthaidev@gmail.com>
2024-12-12 09:20:25 +0700

    update

--- .custom_environment.sh
+++ .custom_environment.sh
@@ -3,8 +3,8 @@
 # go install golang.org/dl/go1.21.0@latest
 # go1.21.0 download
 # go1.21.0 version
-alias go="go1.21.0"
-alias makegen="go generate ./..."
+#alias go="go1.21.0"
+#alias makegen="go generate ./..."
 
 export PGPASSWD=Postgres!23456
 export PGUSER=postgres
