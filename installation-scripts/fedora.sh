bdd6413165584b8d523e1be409991d69afa22664
tea0112 <ducthaidev@gmail.com>
2025-01-29 20:25:30 +0700

    update

--- installation-scripts/fedora.sh
+++ installation-scripts/fedora.sh
@@ -13,6 +13,7 @@ y)
     sudo dnf -y install dnf-plugins-core
     sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
     sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    flatpak install flathub com.mattjakeman.ExtensionManager
 
     sudo systemctl enable --now docker
     sudo usermod -aG docker "$USER"
