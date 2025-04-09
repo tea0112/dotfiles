537740152c14592611fac2c15102bc83b4b0d5b3
tea0112 <ducthaidev@gmail.com>
2025-04-09 08:09:12 +0700

    update

--- .custom_environment.sh
+++ .custom_environment.sh
@@ -10,3 +10,14 @@ export PGUSER=postgres
 export PGPORT=5432
 export PGDB=postgres
 # export GOPRIVATE=dev.azure.com
+#
+
+if [ -z "${JAVA_HOME}" ]
+then
+    JAVA_HOME=$(readlink -nf $(which java) | xargs dirname | xargs dirname)
+    if [ ! -e "$JAVA_HOME" ]
+    then
+        JAVA_HOME=""
+    fi
+    export JAVA_HOME=$JAVA_HOME
+fi
