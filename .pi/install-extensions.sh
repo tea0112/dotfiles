483040f31c88788463f34b7db59bccbb59316035
tea0112 <ducthaidev@gmail.com>
2026-08-25 22:24:04 +0700

    update

--- .pi/install-extensions.sh
+++ .pi/install-extensions.sh
@@ -3,4 +3,5 @@
 pi install git:github.com/obra/superpowers
 pi install npm:pi-mcp-adapter
 pi install npm:pi-permission-layers
+pi install npm:@narumitw/pi-goal
 npx skills@latest add mattpocock/skills
