b566de2941fb104f7bd688f7233fab1d6a3d1c4e
tea0112 <ducthaidev@gmail.com>
2026-08-26 10:32:23 +0700

    update

--- .pi/install-extensions.sh
+++ .pi/install-extensions.sh
@@ -2,6 +2,4 @@
 
 pi install git:github.com/obra/superpowers
 pi install npm:pi-mcp-adapter
-pi install npm:pi-goal-list-loop-audit
-pi install npm:@juicesharp/rpiv-ask-user-question
 npx skills@latest add mattpocock/skills
